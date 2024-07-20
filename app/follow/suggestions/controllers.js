052bb9256477c7e4d62f6bad58ec8c56edc3b7b7
Khafiz <khafiz@MacBook-Pro-Khafiz.local>
2024-07-20 14:44:02 +0500

    improve

--- app/follow/suggestions/controllers.js
+++ app/follow/suggestions/controllers.js
@@ -1,51 +1,43 @@
-const Follow = require('../Follow');
 const User = require('../../auth/User');
-const sequelize = require('sequelize');
+const Follow = require('../Follow'); 
 
+const { Op, fn } = require('sequelize');
 const suggestions = async (req, res) => {
     try {
-        const follows = await Follow.findAll({
-            where: {
-                user_id: req.user.id
-            },
-            order: [
-                [sequelize.fn('RANDOM')]  
-            ],
+        const followedUsers = await Follow.findAll({
+            attributes: ['user_id'],
+            where: { follower_id: req.user.id },
+            group: ['user_id'],
+            order: [fn('RANDOM')],
             limit: 5
         });
 
-        const ids = follows.map(item => item.follower_id);
-        console.log(ids);
+        const followedUserIds = followedUsers.map(follow => follow.user_id);
 
-        const users = await User.findAll({
-            where: {
-                id: ids
-            },
-            include: [
-                {
-                    model: Follow,
-                    as: 'following',
-                    separate: true,
-                    order: [['id', 'DESC']],
-                    limit: 1,
-                    attributes: ['follower_id'] 
-                }
-            ]
+        if (followedUserIds.length === 0) {
+            return res.status(200).send([]);
+        }
+
+        const randomFollowersPromises = followedUserIds.map(async (userId) => {
+            const randomFollowers = await Follow.findAll({
+                attributes: ['follower_id'],
+                where: { user_id: userId },
+                order: [fn('RANDOM')],
+                limit: 1
+            });
+
+            return randomFollowers.length > 0 ? randomFollowers[0].follower_id : null;
         });
 
-        const arr = users
-            .filter(user => user.following.length > 0)  
-            .map(user => user.following[0].follower_id);
+        const randomFollowerIds = await Promise.all(randomFollowersPromises);
 
-       
+        const usersPromises = randomFollowerIds.filter(id => id !== null).map(id => 
+            User.findOne({ where: { id } })
+        );
 
-        const  recommendationUser  = await User.findAll({
-            where:{
-                id : arr
-            }
-        })
-        res.status(200).send(recommendationUser);
+        const users = await Promise.all(usersPromises);
 
+        res.status(200).send(users);
 
     } catch (error) {
         console.error('Error fetching suggestions:', error);
